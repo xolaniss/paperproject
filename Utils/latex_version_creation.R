f1296dc8ac0f942c9d2c00ff7e93bd844b6a7fc4
Xolani Sibande <xolaniss@gmail.com>
2024-07-15 19:38:18 +0200

    updated latex_version script to include overwriting

--- Utils/latex_version_creation.R
+++ Utils/latex_version_creation.R
@@ -7,17 +7,21 @@ library(tidyverse)
 library(fs)
 
 
+# Delete current latex folder ----------------------------------------
+if (dir.exists(here("Latex_version"))) {
+  dir_delete(here("Latex_version"))
+}
+
 # Create folder ------------------------------------------------------
 dir.create(here("Latex_version"))
 
 # Copy text files ---------------------------------------------------------
-list.files(path = ".", pattern = "tex", full.names = TRUE, ) %>%
-  file.copy(to = here("Latex_version"))
-file.copy(here("references.bib"), here("Latex_version"))
+list.files(path = ".", pattern = "tex", full.names = TRUE ) %>%
+  file.copy(to = here("Latex_version"), overwrite = TRUE)
+file.copy(here("references.bib"), here("Latex_version"), overwrite = TRUE)
 
 # Copy resources folder ---------------------------------------------
 folder_path <- list.dirs(path = ".", recursive = FALSE) %>%
   str_subset("files")
 folder_name <- str_remove(folder_path, "./")
 dir_copy(folder_path, here("Latex_version", folder_name), overwrite = TRUE)
-
